9fbafa2a459dd649237861902fb56ef9b4452089
killsystem10 <killsystem10@outlook.com>
2025-04-02 11:04:00 +0900

    이론과제3 완료

--- temp/test.c
+++ temp/test.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
-main() {
-	printf("test");
+int main() {
+	unsigned char a = 0xcd;
+	unsigned char b = 0xf0;
+	unsigned char c;
 
+	printf("z=%02x\n", c=a^b);
+	printf("z=%02x", c=~a|b<<2);
+	/*
+	a = 1000000101
+	b = 1001000000
+	    0001000101 = a^b = 45
+
+		0111111010 = ~a
+		0100000000 = b<<2
+		0111111010 = ~a|b<<2 = 1fa
+
+	*/
 }
