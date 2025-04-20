4dc1480106be56eb928ac39d4d2c100ef0802b23
killsystem10 <killsystem10@outlook.com>
2025-04-20 23:06:43 +0900

    이론과제5 테스트

--- temp/test.c
+++ temp/test.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
+int data = 100;
 int main() {
-	unsigned char a = 0xcd;
-	unsigned char b = 0xf0;
-	unsigned char c;
-
-	printf("z=%02x\n", c=a^b);
-	printf("z=%02x", c=~a|b<<2);
-	/*
-	a = 1000000101
-	b = 1001000000
-	    0001000101 = a^b = 45
-
-		0111111010 = ~a
-		0100000000 = b<<2
-		0111111010 = ~a|b<<2 = 1fa
-
-	*/
+	int data = 10;
+	int i;
+	for (i = 0;i < data;i++) {
+		int data = 1;
+		data += 3;
+		if (data > 10) {
+			break;
+		}
+	}
+	printf("%d", data);
 }
